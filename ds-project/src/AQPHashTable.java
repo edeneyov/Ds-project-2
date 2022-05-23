6ddb1291ab79ae88e2e8413bc379160cbddaac06
reshitd <100790376+reshitd@users.noreply.github.com>
2022-05-23 17:33:04 +0300

    Update AQPHashTable.java

--- ds-project/src/AQPHashTable.java
+++ ds-project/src/AQPHashTable.java
@@ -1,15 +1,20 @@
 import java.util.Random;
 
 public class AQPHashTable extends OAHashTable {
-
+	ModHash hash;
+	int m;
 	public AQPHashTable(int m, long p) {
 		super(m);
-		// TODO Complete hash table constructor.
+		this.m = m;
+		hash = new ModHash(m, p);
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
-		// TODO implement hash function
-		return 0;
+		if (i%2==0) {
+			return (hash.Hash(x)+i^2)% m;
+		}
+		return (hash.Hash(x)-i^2)% m;
 	}
 }
+
