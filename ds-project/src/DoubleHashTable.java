837d6f0e6e5d6acaa0ca9b9b6a63952ac7989767
reshitd <100790376+reshitd@users.noreply.github.com>
2022-05-23 17:33:43 +0300

    Update DoubleHashTable.java

--- ds-project/src/DoubleHashTable.java
+++ ds-project/src/DoubleHashTable.java
@@ -1,16 +1,19 @@
 import java.util.Random;
 
 public class DoubleHashTable extends OAHashTable {
-	
+	ModHash hash1;
+	ModHash hash2;
+	int m;
 	public DoubleHashTable(int m, long p) {
 		super(m);
-		// TODO Complete hash table constructor.
+		this.m = m;
+		hash1 = new ModHash(m, p);
+		hash2 = new ModHash(m, p);
 	}
 	
 	@Override
 	public int Hash(long x, int i) {
-		// TODO implement hash function
-		return 0;
+		return (hash1.Hash(x)+i*hash2.Hash(x)(x))%m;
 	}
 	
 }
